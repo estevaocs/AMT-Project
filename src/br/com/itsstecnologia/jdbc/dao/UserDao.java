1cb9ce78177e6ea7318fc0883ccf9747d51aa597
estevaocs <estevao_cs@hotmail.com>
2017-02-17 15:05:58 -0200

    Edit, remove e criação de ArrayList no DAO

--- src/br/com/itsstecnologia/jdbc/dao/UserDao.java
+++ src/br/com/itsstecnologia/jdbc/dao/UserDao.java
@@ -2,33 +2,35 @@ package br.com.itsstecnologia.jdbc.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Calendar;
 
 import br.com.itsstecnologia.jdbc.ConnectionFactory;
 import br.com.itsstecnologia.jdbc.model.User;
 
 public class UserDao {
-	
+
 	private Calendar cal;
-	
-	//conecxao com o banco de dados 
+
+	// conecxao com o banco de dados
 	private Connection connection;
-	
+
 	public UserDao() {
 		this.connection = new ConnectionFactory().getConnection();
 	}
-	
+
 	public void add(User user) {
 		String sql = "insert into usuario "
 				+ "(id_user,login,password,first_name,last_name,dt_birth,tel,email,sex,area)"
 				+ " values (?,?,?,?,?,?,?,?,?,?)";
 
 		try {
-			//Prepared Statement para inserção
+			// Prepared Statement para inserção
 			PreparedStatement stmt = connection.prepareStatement(sql);
 
-			//set os valores
+			// set os valores
 			stmt.setLong(1, user.getId());
 			stmt.setString(2, user.getLogin());
 			stmt.setString(3, user.getPassword());
@@ -40,8 +42,8 @@ public class UserDao {
 			stmt.setString(8, user.getEmail());
 			stmt.setString(9, user.getSex());
 			stmt.setInt(10, user.getArea());
-			
-			//executa
+
+			// executa
 			stmt.execute();
 			stmt.close();
 
@@ -49,5 +51,108 @@ public class UserDao {
 			throw new RuntimeException(e);
 		}
 	}
+	
+	public boolean login(String login, String password) {
+		String sql = "select * from usuario where login=? and password=?";
+		boolean status = false;
+		
+		try {
+			
+			PreparedStatement stmt = connection.prepareStatement(sql);
+			
+			stmt.setString(1, login);
+			stmt.setString(2, password);
+			
+			ResultSet rs = stmt.executeQuery();
+			
+			if(rs.next()) {
+				status = true;
+			}
+			
+			rs.close();
+	        stmt.close();
+			return status;
+			
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+		
+	}
+	
+	public ArrayList<User> getLista() {
+	     try {
+	         ArrayList<User> users = new ArrayList<User>();
+	         PreparedStatement stmt = this.connection.
+	                 prepareStatement("select * from usuario");
+	         ResultSet rs = stmt.executeQuery();
+	 
+	         while (rs.next()) {
+	             // criando o objeto user
+	        	 long id = rs.getLong("id_user");
+	        	 String login = rs.getString("login");
+	        	 String password = rs.getString("password");
+	        	 String name = rs.getString("first_name");
+	        	 String lastName = rs.getString("last_name");
+	        	 Calendar data = Calendar.getInstance();
+	        	 data.setTime(rs.getDate("dt_birth"));
+	        	 String tel =  rs.getString("tel");
+	        	 String email =  rs.getString("email");
+	        	 String sex = rs.getString("sex");
+	        	 int area = rs.getInt("area");
+	        	 
+	             User user = new User(id, login, password, name, lastName, data, tel, email, sex, area);
+	 
+	             // adicionando o objeto à lista
+	             users.add(user);
+	         }
+	         rs.close();
+	         stmt.close();
+	         return users;
+	     } catch (SQLException e) {
+	         throw new RuntimeException(e);
+	     }
+	 }
 
+	public void edit(User user) {
+		String sql = "update usuario set login,password = ?, first_name = ?,"
+				+ " last_name = ?, dt_birth = ?, tel =?, email = ?, sex = ?,"
+				+ " area = ? where id_user = ?";
+
+		try {
+			// Prepared Statement para inserção
+			PreparedStatement stmt = connection.prepareStatement(sql);
+
+			// set os valores
+			stmt.setLong(10, user.getId());
+			stmt.setString(1, user.getLogin());
+			stmt.setString(2, user.getPassword());
+			stmt.setString(3, user.getFirstName());
+			stmt.setString(4, user.getLastName());
+			user.getCalendarBirth();
+			stmt.setDate(5, new java.sql.Date(Calendar.getInstance().getTimeInMillis()), cal);
+			stmt.setString(6, user.getTel());
+			stmt.setString(7, user.getEmail());
+			stmt.setString(8, user.getSex());
+			stmt.setInt(9, user.getArea());
+
+			// executa
+			stmt.execute();
+			stmt.close();
+
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public void remove(User user) {
+	     try {
+	         PreparedStatement stmt = connection
+	                 .prepareStatement("delete from usuario where id_user=?");
+	         stmt.setLong(1, user.getId());
+	         stmt.execute();
+	         stmt.close();
+	     } catch (SQLException e) {
+	         throw new RuntimeException(e);
+	     }
+	 }
 }
--- src/br/com/itsstecnologia/jdbc/teste/TestaInsercao.java
+++ src/br/com/itsstecnologia/jdbc/teste/TestaInsercao.java
@@ -1,34 +1,31 @@
 package br.com.itsstecnologia.jdbc.teste;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
+import java.util.Scanner;
 
 import br.com.itsstecnologia.jdbc.dao.UserDao;
-import br.com.itsstecnologia.jdbc.model.User;
 
 public class TestaInsercao {
 
 	public static void main(String[] args) {
-		try {
-			String data = "13/03/1995";
-			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-			Calendar cal = Calendar.getInstance();
-			cal.setTime(sdf.parse(data));
-
-			User usuario = new User(1, "admin", "admin", "Estevão", "Cristino", cal, "(62) 9939-29119",
-					"estevao.silva@itsstecnologica.com.br", "M", 1);
-
-			// grave nessa conexão!!!
-			UserDao dao = new UserDao();
-
-			// método elegante
-			dao.add(usuario);
-
-			System.out.println("Gravado!");
-		} catch (ParseException e) {
-			e.printStackTrace();
+		String login;
+		String password;
+		
+		Scanner scn = new Scanner(System.in);
+		UserDao dao = new UserDao();
+		
+		System.out.println("login: ");
+		login = scn.nextLine();
+		
+		System.out.println("password: ");
+		password = scn.nextLine();
+		
+		if(dao.login(login, password)) {
+			System.out.println("logado com sucesso");
+		} else {
+			System.out.println("usuario ou senha invalido");
 		}
+		
+		scn.close();
 	}
 	
 }
